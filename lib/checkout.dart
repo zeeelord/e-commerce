87e502f87602fb044949913b7d0a680fa82a89dd
Oladayo <oladayoafolabi@outlook.com>
2025-08-27 19:53:13 +0100

    final_

--- lib/checkout.dart
+++ lib/checkout.dart
@@ -6,30 +6,62 @@ class Product {
   final String name;
   final double price;
   final int id;
-// getting the name first character and telling it start from  the first letter and end at it
   String get initial => name.substring(0, 1);
-// getting the display name to display the content  product.display  i.e = name: 'apples  price: 1.60
-  String get displayName => '($initial) $name.substring()}: \$$price';
+  String get displayName => '($initial) ${name.substring(1)} \$$price';
+  // this outputs something like this (A) name
 }
 
+// getting the name first character and telling it start from  the first letter and end at it
+// getting the display name to display the content  product.display  i.e = name: 'apples  price: 1.60
+
 class Item {
-  Item({required this.product, this.quantity = 1});
+  // ___________________________________________
+  // this checks or item quantity
+  Item({required this.product, this.quantity = 1}); // default quanity is 1
   final Product product;
   final int quantity;
   double get price => quantity * product.price;
+  String toString() {
+    return '$quantity x {product.name}: \$$price';
+  }
 }
 
 class Cart {
   final Map<int, Item> _items = {};
   void addProduct(Product product) {
+    //  Product(name: 'apples', id: 1, price: 1.60),
+    //
     final item = _items[product.id];
+    //items[1] ;
     if (item == null) {
-      _items[product.id] = Item(product: product, quantity: 1);
+      // check if theres not any item
+      _items[product.id] =
+          Item(product: product, quantity: 1); //items[1]= Items() ;
     } else {
-      _items[product.id] = Item(product: product, quantity: item.quantity + 1);
+      //  if there is add one to it
+      _items[product.id] = Item(
+          product: product,
+          quantity: item.quantity +
+              1); // adding this to the _item in a sense  item[product.id(key)=Item(product: product, quantity: item.quantity + 1)(which is the value) ]
     }
   }
+
+  bool get IsEmpty => _items.isEmpty;
+
+  double total() => _items.values
+      .map((item) => item.price)
+      .reduce((value, element) => value + element);
+
+  String toString() {
+    if (_items.isEmpty) {
+      return 'cart is empty';
+    }
+    final itemizedList =
+        _items.values.map((item) => item.toString()).join('\n');
+    return '$itemizedList \nTotal: \$total()';
+  }
 }
+
 // loop
 //prompt view cart/ add item/ checkout
 //if selection  ==add item
@@ -67,7 +99,34 @@ Product? chooseProduct() {
   return null;
 }
 
+bool checkout(Cart cart) {
+  if (cart.IsEmpty) {
+    print('cart is empty');
+    return false;
+  }
+  final total = cart.total();
+  print('Total:\$$total');
+  stdout.write('payment in cash');
+  final line = stdin.readLineSync();
+  if (line == null || line.isEmpty) {
+    return false;
+  }
+  final paid = double.tryParse(line); //] converts a string to a double number
+  if (paid == null) {
+    return false;
+  }
+  if (paid >= total) {
+    final change = paid - total;
+    print("Change : \$${change.toStringAsFixed(2)}");
+    return true;
+  } else {
+    print("Not enough cash ");
+    return false;
+  }
+}
+
 void main() {
+  final cart = Cart();
   while (true) {
     stdout.write(
         'What do you want to do? (v)iew items, (a)dd items, (c)heckout:');
@@ -75,12 +134,15 @@ void main() {
     if (line == "a") {
       final product = chooseProduct();
       if (product != null) {
-        print(product.displayName);
+        cart.addProduct(product);
+        print(cart);
       }
     } else if (line == 'v') {
-      //TODO: Implement
+      print(cart);
     } else if (line == 'c') {
-      //TODO: Implement
+      if (checkout(cart)) {
+        break;
+      }
     }
   }
 }
